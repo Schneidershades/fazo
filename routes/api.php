81647c23401ba8f9f142e12d925ea740e435da5d
Schneider Shades <schneidershades@gmail.com>
2021-03-11 13:00:10 +0100

    push

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-
 use Illuminate\Support\Facades\Route;
 
 Route::resource('flight-check', 'Api\Flight\FlightCheckController');
