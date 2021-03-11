a2bacc4545d7fb7341eb1c809ca1cf42b97d2e87
Schneider Shades <schneidershades@gmail.com>
2021-03-11 12:49:28 +0100

    nini

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
 use Illuminate\Support\Facades\Route;
 
 Route::resource('flight-check', 'Api\Flight\FlightCheckController');
