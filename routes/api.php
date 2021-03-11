5269b49b686a3d82fad0ac45e810571c8128cd50
Schneider Shades <schneidershades@gmail.com>
2021-03-11 13:54:08 +0100

    ni

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::resource('flight-check', 'Api\Flight\FlightCheckController');
-Route::resource('flight-select', 'Api\Flight\FlightSelectController');
+Route::resource('travel-selecting', 'Api\Flight\FlightSelectController');
 Route::resource('flight-book', 'Api\Flight\FlightBookingController');
 Route::resource('flight-ticket', 'Api\Flight\FlightTicketController');
