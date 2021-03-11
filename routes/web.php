05f0a78262eaa9bc612a5d30d7493535b8c05d6c
Schneider Shades <schneidershades@gmail.com>
2021-03-11 13:50:54 +0100

    push

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Auth::routes();
-
-// Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::post('flight-check', 'Web\Flight\FlightCheckController@store')->name('flight-check');
-
-Route::get('flight-list', 'Web\Flight\FlightListController@store')->name('flight-list');
